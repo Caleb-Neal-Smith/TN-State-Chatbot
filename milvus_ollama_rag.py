7009212538b90c3b2efc60a12849de5cff8080ea
Caleb Smith <cnsmith0611@gmail.com>
2025-02-01 15:25:03 -0600

    Modified RAG agent to work with Milvus Standalone and Llama Parse

--- milvus_ollama_rag.py
+++ milvus_ollama_rag.py
@@ -1,30 +1,43 @@
-import arxiv
+# import arxiv
 from llama_index.core import SimpleDirectoryReader
 from llama_index.vector_stores.milvus import MilvusVectorStore
 from llama_index.core import VectorStoreIndex, Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from pymilvus import MilvusClient
+# from pymilvus import MilvusClient
+from dotenv import load_dotenv
+load_dotenv()
+from llama_parse import LlamaParse
+
+# set up parser
+parser = LlamaParse(
+    result_type="text"  # "markdown" and "text" are available
+)
 
 dir_name = "./Documents/pdf_data/"
 
-arxiv_client = arxiv.Client()
-paper = next(arxiv.Client().results(arxiv.Search(id_list=["1706.03762"])))
+# use SimpleDirectoryReader to parse our file
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader(input_files=[f"{dir_name}RFI 30901-57524 Project ARIS FINAL (3).pdf"], file_extractor=file_extractor).load_data()
+print(documents)
+
+# arxiv_client = arxiv.Client()
+# paper = next(arxiv.Client().results(arxiv.Search(id_list=["1706.03762"])))
 
 # Download the PDF to a specified directory with a custom filename.
-paper.download_pdf(dirpath=dir_name, filename="attention.pdf")
+# paper.download_pdf(dirpath=dir_name, filename="attention.pdf")
 
 vector_store = MilvusVectorStore(
-    uri="./milvus_rag_demo.db",  dim=768, overwrite=True
+    uri="http://localhost:19530",  dim=768, overwrite=True
 )
 
 embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 llm = Ollama(model="llama3.2",temperature=0.1, request_timeout=480.0)
 
-pdf_document = SimpleDirectoryReader(
-    input_files=[f"{dir_name}attention.pdf"]
-).load_data()
-print("Number of Input documents:", len(pdf_document))
+# pdf_document = SimpleDirectoryReader(
+#     input_files=[f"{dir_name}RFI 30901-57524 Project ARIS FINAL (3).pdf"]
+# ).load_data()
+# print("Number of Input documents:", len(pdf_document))
 
 # OR execute this command if you have multiple PDFs inside the directory
 # pdf_document = SimpleDirectoryReader(
@@ -43,7 +56,7 @@ Settings.embed_model = embedding_model
 Settings.chunk_size = 128
 Settings.chunk_overlap = 64
 
-index = VectorStoreIndex.from_documents(pdf_document)
+index = VectorStoreIndex.from_documents(documents)
 print("Number of nodes:", len(index.docstore.docs))
 
 query_engine = index.as_query_engine()
@@ -53,10 +66,14 @@ Output:
 Number of nodes: 196
 """
 
-query = input("Ask a question about the research paper 'Attention is All You Need': ")
+choice = 0
+while(choice != 1):
+    query = input("Ask a question about Tennessee State Treasury: ")
 
-result = query_engine.query(query)
-print(result)
+    result = query_engine.query(query)
+    print(result)
+
+    choice = int(input("Enter 0 to ask another question or 1 to exit: "))
 """
 Output:
 Multi-head attention allows the model to jointly attend to information from different representation subspaces at different positions. With a single attention head, averaging inhibits this.
