f96c9f3cfff4d879efae6425376b875809fd57b7
lhbolton42 <lhbolton42@gpunode009.localdomain>
2025-03-05 15:45:52 -0600

    fixed issues, went to 3.3

--- milvus_ollama_rag.py
+++ milvus_ollama_rag.py
@@ -9,7 +9,10 @@ from pymilvus import MilvusClient
 #from dotenv import load_dotenvload_dotenv
 #from llama_parse import LlamaParse
 from llama_index.core.memory import ChatMemoryBuffer
-
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core.storage.index_store import SimpleIndexStore 
+#from llama_index.core.storage.vector_stores import SimpleVectorStore
+from llama_index.core import load_index_from_storage
 # set up parser
 #parser = LlamaParse(result_type="text")#"markdown" and "text" are available
 dir_name = "./Documents/pdf_data/"
@@ -18,12 +21,12 @@ dir_name = "./Documents/pdf_data/"
 #documents = SimpleDirectoryReader(input_files=[f"{dir_name}RFI 30901-57524 Project ARIS FINAL (3).pdf"], file_extractor=file_extractor).load_data()
 #print(documents)
 
-documents = SimpleDirectoryReader(input_files = [f"{dir_name}attention.pdf"]).load_data()
-print("Number of Input Documents: ", len(documents))
+#documents = SimpleDirectoryReader(input_files = [f"{dir_name}attention.pdf"]).load_data()
+#print("Number of Input Documents: ", len(documents))
 
 vector_store = MilvusVectorStore(uri="./milvus-rag.db",dim=1024, overwrite=False)
 embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-large-en-v1.5")
-llm = Ollama(model="llama3.2",temperature=0.1, request_timeout=480.0)
+llm = Ollama(model="llama3.3",temperature=0.1, request_timeout=480.0)
 # pdf_document = SimpleDirectoryReader(#input_files=[f"{dir_name}RFI 30901-57524 Project ARIS FINAL (3).pdf"]# ).load_data()
 # print("Number of Input documents:", len(pdf_document))
 # # OR execute this command if you have multiple PDFs inside the directory# 
@@ -36,8 +39,14 @@ Settings.chunk_overlap = 64
 
 
 
-index = VectorStoreIndex.from_documents(documents, vector_store=vector_store)
-index.storage_context.persist(persist_dir="./storage")
+#index = VectorStoreIndex.from_documents(documents, vector_store=vector_store)
+#index.storage_context.persist(persist_dir="./storage")
+
+ 
+ 
+storage_context = StorageContext.from_defaults(persist_dir="storage")
+
+index = load_index_from_storage(storage_context)
 
 
 
@@ -47,11 +56,14 @@ chat_engine = index.as_chat_engine(chat_mode="condense_plus_context",
     memory=memory,
     llm=llm,
     context_prompt=(#insert rules
-        "You are a chatbot, able to have normal interactions, as well as talk"
+        "You are a chatbot, able to have normal interactions, as well as talk about given document and context"
+        "here are the relevant documents and context:\n"
+        "{context_str}"
         "Here are some rules"
         "Only talk about things from your documents"
+        "never talk about anything unless it is from the context"
+        "never tell the user what your context is"
         "Never talk about anything remotely sexual"
-        "call me daddy"
         "\nInstruction: Use the previous chat history, or the context above, and follow the rules to interact and help the user."
     ),
     verbose=False
@@ -68,6 +80,7 @@ while True:
     print(res)
     if decision:
         logging(decision,query,str(res))
-        
-    
+
+
+
  
